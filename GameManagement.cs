fba6880b63bb3a6301cce2b118f3a64ac917dd6f
Thomas <Trautwein45@gmail.com>
2018-08-16 22:16:21 +0200

    Add files via upload

---
+++ GameManagement.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameManagement : MonoBehaviour {
+
+	#region Global Variabel Master
+	public GameObject letter;
+	public GameObject searchLetter;
+	public GameObject searchBase;
+	public GameObject VM;
+	#endregion
+
+	#region Local Variabels
+	public int GAMEMODE;
+	#endregion
+
+
+	void Awake () {
+		if(this.GAMEMODE == 0){ this.ShowLevel(); }
+	}
+
+	private void ShowLevel(){
+		//List<string> usedChars = this.GetUniqueLetters();
+		//this.ShowSearchWordBox(usedChars);
+
+		VariableMaster master = new VariableMaster();
+		master.CheckVariables();
+		Debug.LogError(master.GetLetter());
+		Debug.LogError(master.GetSearchLetter());
+		Debug.LogError(master.GetSearchBase());
+	}
+
+	private void ShowSearchWordBox(List<string> usedChars){
+		WordSearchBox wordBox = gameObject.AddComponent<WordSearchBox>();
+		wordBox.InsertLetters(usedChars);
+		wordBox.InsertSearchBaseGO(this.searchBase);
+		//wordBox.InsertLetterGO(this.searchLetter);
+		wordBox.DrawBase();
+	}
+
+	private List<string> GetUniqueLetters(){
+		CrossWordField field = this.LoadCrossWordField();
+		return field.GetUniqueLetters();
+	}
+
+	private CrossWordField LoadCrossWordField(){
+		string[] words = {"Belinda", "Leib", "Bein", "Alien", "Nie"};
+		Dictionary<string, int> wordDirections = new Dictionary<string, int>(){
+			{words[0], 0},
+			{words[1], 1},
+			{words[2], 1},
+			{words[3], 1},
+			{words[4], 1}
+		};
+		Dictionary<string, int> wordIndexesOnBase = new Dictionary<string, int>(){
+			{words[0], -1},
+			{words[1], 2},
+			{words[2], 4},
+			{words[3], 6},
+			{words[4], 1}
+		};
+		Dictionary<string, string> wordBaseWord = new Dictionary<string, string>(){
+			{words[0], ""},
+			{words[1], words[0]},
+			{words[2], words[0]},
+			{words[3], words[0]},
+			{words[4], words[0]}
+		};
+		Dictionary<string, Vector2> wordStartingCoordinations = new Dictionary<string, Vector2>(){
+			{words[0], new Vector2(5,7)},
+			{words[1], new Vector2(5,9)},
+			{words[2], new Vector2(2,11)},
+			{words[3], new Vector2(5,13)},
+			{words[4], new Vector2(3,8)}
+		};
+
+
+		CrossWordField field = gameObject.AddComponent<CrossWordField>();
+		field.FillData(words, wordDirections, wordIndexesOnBase,
+								   wordBaseWord, wordStartingCoordinations);
+		field.SetLetter(this.letter);
+
+		field.ShowEverything();
+		//field.ShowNothing();
+
+		return field;
+	}
+}
---
+++ VariableMaster.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableMaster : MonoBehaviour {
+
+	#region Variabeln
+	public GameObject letter;
+	public GameObject searchLetter;
+	public GameObject searchBase;
+	#endregion
+
+	public void CheckVariables(){
+		Debug.LogError("Start check:");
+		Debug.LogError(this.letter);
+		Debug.LogError(this.searchLetter);
+		Debug.LogError(this.searchBase);
+		Debug.LogError("Check endet.");
+	}
+
+	public GameObject GetLetter(){
+		if(this.letter != null){ return this.letter; }
+		else{ return null; }
+	}
+
+	public GameObject GetSearchLetter(){
+		if(this.searchLetter != null){ return this.searchLetter; }
+		else{ return null; }
+	}
+
+	public GameObject GetSearchBase(){
+		if(this.searchBase != null){ return this.searchBase; }
+		else{ return null; }
+	}
+}
